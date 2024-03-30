e18feea482dd95a8dab1b8e5dc849284cb861693
Piotr Cierczek <165421976+PiotrC02@users.noreply.github.com>
2024-03-30 17:48:24 +0100

    Delete Zadania lab4 directory

--- Zadania lab4/Rozgrzewka/1abc.py
+++
@@ -1,8 +0,0 @@
-import math
-i=float(input('Podaj liczbę:'))
-print('Pierwiastek liczby',i,'=',math.sqrt(i))
-print('jej logarytm =', math.log(i))
-print('Obwód kola o promieniu',i,'=',2*math.pi*i)
-help (math)
-print('dist(p, q, /)')
-print('Return the Euclidean distance between two points p and q.')
