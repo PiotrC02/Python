5491a671b620a5e5eb6e1254a562d969fe1e1643
Piotr Cierczek <165421976+PiotrC02@users.noreply.github.com>
2024-03-30 17:48:13 +0100

    Delete Zadania lab2 directory

--- Zadania lab2/lab2_3.py
+++
@@ -1,8 +0,0 @@
-n=int(input("Podaj rok:"))
-
-if(n%400 == 0) and (n % 100 == 0):
-    print("podany rok jest rokiem przestępnym".format(n))
-elif(n%4 == 0) and (n%100 != 0):
-    print("podany rok jest rokiem przestępnym".format(n))
-else:
-    print("podany rok nie jest rokiem przestępnym".format(n))
